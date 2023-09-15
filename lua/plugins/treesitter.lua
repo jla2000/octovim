1b5e5b9de76edaa2879eb8904fabddc87ac55eb5
Jan Lafferton <jan@lafferton.de>
2023-09-15 23:33:57 +0200

    Configure treesitter text objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
     config = true,
@@ -16,25 +17,52 @@ return {
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
+      textobjects = {
+        select = {
+          enable = true,
 
-      highlight = {
-        enable = true,
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
 
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ia"] = "@parameter.outer",
+            -- You can also use captures from other query groups like `locals.scm`
+            -- nvim_buf_set_keymap) which plugins like which-key display
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]a"] = "@parameter.outer",
+          },
+          goto_next_end = {
+            ["]A"] = "@parameter.outer",
+          },
+          goto_previous_start = {
+            ["[a"] = "@parameter.outer",
+          },
+          goto_previous_end = {
+            ["[A"] = "@parameter.outer",
+          }
+        }
+      }
     }
   }
 }
