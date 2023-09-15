08a50d4273934f6b22af741f0c6061077ccf39ad
Jan Lafferton <jan@lafferton.de>
2023-09-15 23:35:16 +0200

    Add options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,6 +4,7 @@ vim.g.maplocalleader = " "
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 4
 
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
